86fe05a932f0634e560e4839d9552328a43f3f40
kuishou68 <1569339843@qq.com>
2021-10-22 09:39:27 +0800

    fix:2020.10.22 配置跨全端图表组件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,10 +5,14 @@
 		<fix-window />
 		<!-- #endif -->
     </scroll-view>
+	<view class="charts-box">
+	    <qiun-data-charts type="column" :chartData="chartData" />
+	</view>
 </template>
 
 <script>
 	import { version } from '../../package.json'
+	
     export default {
         data() {
             return {
@@ -16,7 +20,19 @@
 			}
         },
         onLoad() {},
-        methods: {}
+        methods: {},
+		mountd(){
+			chartData:{
+			  categories: ["2016", "2017", "2018", "2019", "2020", "2021"],
+			  series: [{
+			    name: "目标值",
+			    data: [35, 36, 31, 33, 13, 34]
+			  }, {
+			    name: "完成量",
+			    data: [18, 27, 21, 24, 6, 28]
+			  }]
+			}
+		}
     }
 </script>
 
