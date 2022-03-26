eff355af12a5bce24f8f9f898ce70065ddb4ef5d
kuishou68 <1569339843@qq.com>
2022-03-26 23:51:34 +0800

    optimize: ①优化首页;

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,9 +5,9 @@
 		<fix-window />
 		<!-- #endif -->
     </scroll-view>
-	<view class="charts-box">
+	<!-- <view class="charts-box">
 	    <qiun-data-charts type="column" :chartData="chartData" />
-	</view>
+	</view> -->
 </template>
 
 <script>
@@ -22,16 +22,16 @@
         onLoad() {},
         methods: {},
 		mountd(){
-			chartData:{
-			  categories: ["2016", "2017", "2018", "2019", "2020", "2021"],
-			  series: [{
-			    name: "目标值",
-			    data: [35, 36, 31, 33, 13, 34]
-			  }, {
-			    name: "完成量",
-			    data: [18, 27, 21, 24, 6, 28]
-			  }]
-			}
+			// chartData:{
+			//   categories: ["2016", "2017", "2018", "2019", "2020", "2021"],
+			//   series: [{
+			//     name: "目标值",
+			//     data: [35, 36, 31, 33, 13, 34]
+			//   }, {
+			//     name: "完成量",
+			//     data: [18, 27, 21, 24, 6, 28]
+			//   }]
+			// }
 		}
     }
 </script>
